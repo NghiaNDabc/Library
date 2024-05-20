17e17601d1f515e07405f90f084588d466e5e80b
hoangfcoongminh <hoangfcoongminh@gmail.com>
2024-05-20 17:53:13 +0700

    tao chuc nang dang nhap

---
+++ Controllers/TrangChuController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLySachThuVien.Models;
+
+namespace QuanLySachThuVien.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        private QuanLySachThuVienContext db = new QuanLySachThuVienContext();
+
+        // GET: Saches
+        public ActionResult Index()
+        {
+            var saches = db.Saches.Include(s => s.DanhMuc);
+            return View(saches.ToList());
+        }
+
+        // GET: Saches/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sach sach = db.Saches.Find(id);
+            if (sach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sach);
+        }
+
+        // GET: Saches/Create
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/mapTaiKhoan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySachThuVien.Models
+{
+    public class mapTaiKhoan
+    {
+        public TaiKhoan ChiTiet(string tenDangNhap)
+        {
+            try
+            {
+                QuanLySachThuVienContext db = new QuanLySachThuVienContext();
+                var model = db.TaiKhoans.SingleOrDefault(m=>m.tenDangNhap == tenDangNhap);
+                return model;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
