d3251c603f2bc3277798469d180ca5b4d8278004
Nggocc <bichngoc17072003@gmail.com>
2024-05-21 02:37:42 +0700

    hoan thien 80%

--- Controllers/TrangChuController.cs
+++ Controllers/TrangChuController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using QuanLySachThuVien.Models;
 
@@ -15,12 +16,20 @@ namespace QuanLySachThuVien.Controllers
         private QuanLySachThuVienContext db = new QuanLySachThuVienContext();
 
         // GET: Saches
-        public ActionResult Index()
+        public ActionResult Index(string key)
         {
             var saches = db.Saches.Include(s => s.DanhMuc);
+
             return View(saches.ToList());
+
         }
-       
+        public ActionResult XemALLSach()
+        {
+            var saches = db.Saches;
+            return View(saches.ToList());
+
+        }
+
         // GET: Saches/Details/5
         public ActionResult Details(string id)
         {
@@ -36,9 +45,100 @@ namespace QuanLySachThuVien.Controllers
             return View(sach);
         }
 
-        // GET: Saches/Create
-     
+        // GET: Saches/Create\
 
+        public ActionResult XemSachTheoDM(string madm)
+        {
+            var saches = db.Saches.Where(s => s.maDanhMuc == madm).ToList();
+            return View(saches);
+        }
+        public ActionResult TimKiem(string key)
+        {
+            if (!String.IsNullOrEmpty(key.ToLower()))
+            {
+                var saches = db.Saches.Where(s => s.tenSach.ToLower().Contains(key) 
+                || s.tenTacGia.ToLower().Contains(key) || s.namXuatBan.ToString().Equals(key)
+                || s.nhaXuatBan.Contains(key) || s.DanhMuc.tenDanhMuc.ToLower().Contains(key)).ToList();
+
+                if (!saches.Any())
+                {
+                    ViewBag.msg = "Không tìm thấy sách có thông tin liên quan!";
+                }
+                else
+                {
+                    return View(saches);
+
+                }
+            }
+            return View();
+        }
+        public ActionResult ThemVaoGioHang(string maSach)
+        {
+
+            ViewBag.ma = maSach;
+            List<ChiTietGioHang> li = (List<ChiTietGioHang>)Session["giohang"];
+            if (li == null)
+            {
+                li = new List<ChiTietGioHang>();
+                ChiTietGioHang ct = new ChiTietGioHang
+                {
+                    maSach = maSach,
+                    soLuong = 1
+                };
+                li.Add(ct);
+            }
+
+            else
+            {
+                var chiTiet = li.FirstOrDefault(c => c.maSach == maSach);
+                if (chiTiet != null)
+                {
+                    chiTiet.soLuong++;
+                }
+                else
+                {
+                    // Nếu sách chưa tồn tại, thêm mới sách vào giỏ hàng
+                    ChiTietGioHang ct = new ChiTietGioHang
+                    {
+                        maSach = maSach,
+                        soLuong = 1
+                    };
+                    li.Add(ct);
+                }
+            }
+            
+            Session["giohang"]=li;
+            return View();
+        }
+        public ActionResult XoaSanPham(string maSach)
+        {
+            List<ChiTietGioHang> li = (List<ChiTietGioHang>)Session["giohang"];
+            if (li != null)
+            {
+                var chiTiet = li.FirstOrDefault(c => c.maSach == maSach);
+                if (chiTiet != null)
+                {
+                    li.Remove(chiTiet);
+                    Session["giohang"] = li;
+                }
+            }
+            return RedirectToAction("XemGioHang");
+        }
+        public ActionResult XemGioHang()
+        {
+            List<ChiTietGioHang> li = Session["giohang"] as List<ChiTietGioHang> ?? new List<ChiTietGioHang>();
+            return View(li);
+        }
+        public ActionResult MuonSach()
+        {
+            List<ChiTietGioHang> li = (List<ChiTietGioHang>)Session["giohang"];
+            Session.Remove("giohang");
+            return View(li);
+        }
+        public ActionResult QuyDinh()
+        {
+            return View();
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
