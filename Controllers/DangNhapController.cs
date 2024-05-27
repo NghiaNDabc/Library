953e44df7c91ae0a0ee7ba5ff8ffe4bf30a9576a
Nggocc <bichngoc17072003@gmail.com>
2024-05-27 10:55:21 +0700

    Add dangky + modified session

--- Controllers/DangNhapController.cs
+++ Controllers/DangNhapController.cs
@@ -9,9 +9,10 @@ namespace QuanLySachThuVien.Areas.Admin.Controllers
 {
     public class DangNhapController : Controller
     {
-        public ActionResult DangNhap()
+        public ActionResult DangXuat()
         {
-            return View();
+            Session["taikhoan"] = null;
+            return RedirectToAction("DangNhap");
         }
         [HttpPost]
         public ActionResult DangNhap(string tenDangNhap, string matKhau)
@@ -48,7 +49,45 @@ namespace QuanLySachThuVien.Areas.Admin.Controllers
             {
                 return Redirect("/Admin/BaoTriSach/Index");
             }
-            else return Redirect("/TrangChu/Index");
+            else
+            {
+                Session["taikhoan"] = taiKhoan;
+                return Redirect("/TrangChu/Index");
+            }
+        }
+        public ActionResult DangKy(string email, string hoten, string ngaySinh, string queQuan, string sdt, string tenDangNhap, string matKhau, string matKhaunl)
+        {
+            //Kiểm tra xem đã điền đủ thông tin
+            if (string.IsNullOrEmpty(tenDangNhap) || string.IsNullOrEmpty(matKhau)||string.IsNullOrEmpty(email)||
+                string.IsNullOrEmpty(hoten)||string.IsNullOrEmpty(queQuan)||string.IsNullOrEmpty(sdt)||string.IsNullOrEmpty(ngaySinh))
+            {
+                ViewBag.thongBao = "Vui lòng nhập đầy đủ thông tin !";
+                return View();
+            }
+
+            //Tìm tài khoản theo tên đăng nhập trong database
+            QuanLySachThuVienContext db = new QuanLySachThuVienContext();
+            var taiKhoan = db.TaiKhoans.SingleOrDefault(m => m.tenDangNhap == tenDangNhap);
+
+            //Kiểm tra tài khoản tồn tại không
+            if (taiKhoan == null)
+            {
+                ViewBag.thongBao = "Tài khoản hoặc mật khẩu không chính xác !";
+                //code Minh
+                if (matKhau != matKhaunl)
+                {
+                    ViewBag.thongBao = "Tài khoản hoặc mật khẩu không chính xác !";
+                    ViewBag.tenDangNhap = tenDangNhap;
+                    return View();
+                }
+                // xử lý để khi đăng ký xong thì chuyển qua đăng nhập
+            }
+            else
+            {
+                ViewBag.thongBao = "Tên đăng nhập đã tồn tại!";
+                return View();
+            }
+            return View();
         }
 
     }
